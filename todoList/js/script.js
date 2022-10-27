3c4df186746ffc8f397cdce9bde64029922d0dc5
Nazar Koval <n.kovaldev@gmail.com>
2022-10-27 08:43:16 +0300

    enh: added shadows to the span

--- todoList/js/script.js
+++ todoList/js/script.js
@@ -1,30 +1,30 @@
-const addTask = document.getElementById("add-task")
-const inputTask = document.getElementById("input-task")
-const taskContainer = document.getElementById("task-container")
+const addTask = document.querySelector("#add-task")
+const inputTask = document.querySelector("#input-task")
+const taskContainer = document.querySelector("#task-container")
 
 addTask.addEventListener("click", function () {
 	let task = document.createElement("div")
 	task.classList.add("task")
 
-	let li = document.createElement("li")
-	li.innerText = `${inputTask.value}`
+	let li = document.createElement("span")
+	li.innerText = inputTask.value
 	task.appendChild(li)
 
 	let checkButton = document.createElement("button")
-	checkButton.innerHTML = '<i class="fa-solid fa-check"></i>'
 	checkButton.classList.add("check-task")
+	checkButton.innerHTML = `<i class="fa-solid fa-check"></i>`
 	task.appendChild(checkButton)
 
-	checkButton.addEventListener("click", function (e) {
+	checkButton.addEventListener("click", function () {
 		this.parentElement.classList.toggle("line-through")
 	})
 
 	let deleteButton = document.createElement("button")
-	deleteButton.innerHTML = '<i class="fa-solid fa-trash-can"></i>'
 	deleteButton.classList.add("delete-task")
+	deleteButton.innerHTML = `<i class="fa-solid fa-trash-can"></i>`
 	task.appendChild(deleteButton)
 
-	deleteButton.addEventListener("click", function (e) {
+	deleteButton.addEventListener("click", function () {
 		this.parentElement.remove()
 	})
 
